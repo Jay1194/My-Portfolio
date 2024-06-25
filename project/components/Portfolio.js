5e37a4597f153bab9af3997b2fb2b19358a3cfb0
Jayden Taylor <j.thomastt1@gmail.com>
2024-06-25 00:07:05 -0700

    finishing touches

--- project/components/Portfolio.js
+++ project/components/Portfolio.js
@@ -4,13 +4,16 @@ export default function Portfolio(){
             <div className='center'>
                 <h2>Portfolio</h2>
                 <div className='portfolio-container'>
+                    <a href="https://github.com/Jay1194/Tech-Blog"><img src="/images/MVC.png" alt="" /></a>
+                    <a href="https://github.com/Jay1194/Weather-Dashboard"><img src="/images/forcast.png" alt="" /></a>
+                    <a href="https://github.com/Jay1194/Deep-Thoughts"><img src="/images/deep.png" alt="" /></a>
+                   {/* <a href=""><img src="/images/placeholder.jpg" alt="" /></a>
                     <a href=""><img src="/images/placeholder.jpg" alt="" /></a>
-                    <a href=""><img src="/images/placeholder.jpg" alt="" /></a>
-                    <a href=""><img src="/images/placeholder.jpg" alt="" /></a>
-                    <a href=""><img src="/images/placeholder.jpg" alt="" /></a>
-                    <a href=""><img src="/images/placeholder.jpg" alt="" /></a>
-                    <a href=""><img src="/images/placeholder.jpg" alt="" /></a>
+                    <a href=""><img src="/images/placeholder.jpg" alt="" /></a>*/}
                 </div>
+                <div id="More">
+            <a href="https://github.com/Jay1194">See All Projects</a>
+            </div>
             </div>
         </section>
     );
